7a562ab8d324a0cdfd7070d7ae0a83becdda6298
Spencer Wyman <spencer.wyaman7@gmail.com>
2020-01-03 16:50:32 -0800

    first commit, built boilerplate

---
+++ client/components/App.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render () {
+    return (
+      <div>
+        <p>LOREM IPSUM REACTO, DID THIS SHOW?</p>
+      </div>
+    )
+  }
+}
+
+export default App;
