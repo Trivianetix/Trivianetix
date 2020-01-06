a9e91a64ba44ef2aa405839de465e47d4fb166c0
bigboydiamonds <57741810+bigboydiamonds@users.noreply.github.com>
2020-01-06 13:15:35 -0800

    everything working on backend aside from finishing game stats

--- server/controller/userModelController.js
+++ server/controller/userModelController.js
@@ -1,4 +1,6 @@
 const db = require('../data/userModel');
+const fs = require('fs');
+const fetch = require("node-fetch");
 
 const userModelController = {};
 
@@ -43,6 +45,39 @@ userModelController.findUser = (req, res, next) => {
             })
         .catch(err => console.log(err))
 
+}
+
+// used to find games played and correct answers
+userModelController.findStats = (req, res, next) => {
+    const text = `
+        SELECT games_played, correct_answers
+        FROM users
+        WHERE username = '${req.params.username}'
+    `
+    db.query(text)
+        .then(response => {
+            //if the user doesn't exist or username/password is incorrect
+            if (response.rows[0]) {
+                console.log('User ', req.params.username , ' Games played: ', response.rows[0].games_played , ' Correct answers: ', response.rows[0].correct_answers);
+                res.locals.stats = response.rows[0];   
+                next();
+            } else {
+                console.log('Error occurred. Username is not sending properly.');
+                res.send('Error occurred. Username is not sending properly.');
+            }
+        })
+    .catch(err => console.log(err))
+}
+
+userModelController.questions = async (req, res, next) => {
+    const url = 'https://opentdb.com/api.php?amount=10&category=9&type=multiple';
+    await fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            res.locals.results = data.results;
+        })
+        .catch(err => console.log(err));
+    next();
 }
 
 // used for when a user wants to update their information -- stretch feature?
