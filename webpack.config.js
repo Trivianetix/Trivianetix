6e6535ad88e0fbe184219caebebd7f3bdb1ff5a2
Yunho Cho <ycho0502@gmail.com>
2020-01-05 20:15:26 -0800

    is now compatible with windows

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,17 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  entry: './client/',
+  entry: "./client/",
   output: {
-    path: path.resolve(__dirname, 'build'),
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js"
   },
   devServer: {
-    // contentBase: __dirname,
-    publicPath: '/build/',
+    // contentBase: path.resolve(__dirname, "build")
+    publicPath: "/build/",
     proxy: {
-      '/api': 'http://localhost:3000',
-    },
+      "/api": "http://localhost:3000"
+    }
   },
   mode: process.env.NODE_ENV,
   module: {
@@ -20,22 +20,16 @@ module.exports = {
         test: /\.jsx?/,
         exclude: /(node_modules)/,
         use: {
-          loader: 'babel-loader',
+          loader: "babel-loader",
           options: {
-            presets: [
-              '@babel/preset-env',
-              '@babel/preset-react',
-            ],
+            presets: ["@babel/preset-env", "@babel/preset-react"]
           }
         }
       },
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader',
-        ],
-      },
-    ],
-  },
-}
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  }
+};
