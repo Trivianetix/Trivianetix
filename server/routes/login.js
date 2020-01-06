ffda87ba669e8612c17724677f5b126e529b8fae
JJ Friedman <jjfriedman@JJs-MacBook-Pro-New.local>
2020-01-06 14:07:05 -0800

    fixed css routing

--- server/routes/login.js
+++ server/routes/login.js
@@ -7,9 +7,10 @@ const userModelController = require('../controller/userModelController');
 
 login.get('/', (req, res) => {
     res.status(200).sendFile(path.resolve(__dirname, '../../index.html'))
-})
-login.get('/styles.css', (req, res) => {
-    res.status(200).sendFile(path.resolve(__dirname, '../../styles.css'))
+});
+
+login.get('/index.css', (req, res) => {
+    res.status(200).sendFile(path.resolve(__dirname, '../../index.css'))
 })
 
 //should move the login post to here?
